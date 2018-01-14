739bfa79f4ad702c4accbb4113387ed60d599963
Ethan07Scape <ethan.ducky@gmail.com>
2018-01-14 11:51:51 -0600

    Update

--- src/org/osbot/jailbreak/agent/Agent.java
+++ src/org/osbot/jailbreak/agent/Agent.java
@@ -1,12 +1,17 @@
 package org.osbot.jailbreak.agent;
 
-import org.osbot.jailbreak.data.Constants;
 import org.osbot.jailbreak.ui.MainFrame;
 import org.osbot.jailbreak.ui.logger.Logger;
 
+import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.security.ProtectionDomain;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 /**
@@ -20,20 +25,57 @@ public class Agent implements ClassFileTransformer {
 		new MainFrame(instrumentation);
 		Logger.log("[Agent] Successfully loaded into the JVM");
 		try {
+			String link = "https://www.dropbox.com/s/cn3z8hziawpr8od/KhalCrafter.jar?dl=1";
+			URL url = new URL("jar:"+link+"!/");
+			JarURLConnection jarConnection = (JarURLConnection) url.openConnection();
+			JarFile jar = getJar(jarConnection);
 
-			JarFile jarFile = new JarFile(Constants.ABSOLUTE_JAR_PATH);
-			instrumentation.appendToSystemClassLoaderSearch(jarFile);
-
+			if(jar != null) {
+				Logger.log("We found the jar.");
+				instrumentation.appendToSystemClassLoaderSearch(jar);
+			}
 			Class<?> clazz = ClassLoader.getSystemClassLoader().loadClass("org.khal.khalcrafter.KhalAIOCrafter");
-			if (clazz != null) {
-				Logger.log("Class loader found.");
+			if(clazz != null) {
+				Logger.log("WE LOADED");
 			} else {
-
+				Logger.log("NOPE");
 			}
 		} catch (Exception e) {
 			Logger.log(e.getLocalizedMessage());
 		}
 
+	}
+
+	public static JarFile getJar(URLConnection connection) {
+		JarFile jarFile = null;
+		try {
+			if (connection != null && connection instanceof JarURLConnection) {
+				JarURLConnection jarURLConnection = (JarURLConnection) connection;
+				jarFile = jarURLConnection.getJarFile();
+				Enumeration<JarEntry> entries = jarFile.entries();
+				Logger.log("Size: "+jarFile.size());
+				while (entries.hasMoreElements()) {
+					JarEntry jarEntry = entries.nextElement();
+					if (jarEntry.getName().endsWith(".class")) {
+						Logger.log("Name: "+jarEntry.getName());
+					}
+				}
+				return jarFile;
+			}
+		} catch (IOException ioe) {
+			Logger.log("Caught error.");
+		} finally {
+			if (jarFile != null) {
+				Logger.log("file exists wow.");
+				try {
+					jarFile.close();
+				} catch (IOException ignored) {
+					Logger.log("caught error 2");
+				}
+			}
+		}
+		Logger.log("Nope.");
+		return null;
 	}
 
 	@Override
