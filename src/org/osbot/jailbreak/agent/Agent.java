2c945a155f78a09508d36226a1a2eecca588d287
Bautista Baiocchi-Lora <Bautista-Baiocchi-lora@users.noreply.github.com>
2018-01-14 14:13:24 -0300

    Push

--- src/org/osbot/jailbreak/agent/Agent.java
+++ src/org/osbot/jailbreak/agent/Agent.java
@@ -1,12 +1,11 @@
 package org.osbot.jailbreak.agent;
 
+import org.osbot.jailbreak.data.Constants;
 import org.osbot.jailbreak.ui.MainFrame;
 import org.osbot.jailbreak.ui.logger.Logger;
 
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
-import java.net.JarURLConnection;
-import java.net.URL;
 import java.security.ProtectionDomain;
 import java.util.jar.JarFile;
 
@@ -14,37 +13,33 @@ import java.util.jar.JarFile;
  * Created by Ethan on 1/14/2018.
  */
 
-    public class Agent implements ClassFileTransformer {
-        public static void agentmain(String args, Instrumentation instrumentation) {
-            new MainFrame(instrumentation);
-            Logger.log("[Agent] Successfully loaded into the JVM");
-            try {
-                String link = "http://www.link.com/name.jar";
+public class Agent implements ClassFileTransformer {
 
-                URL url = new URL("jar:"+link+"!/");
 
-                JarURLConnection jarConnection = (JarURLConnection) url.openConnection();
-                jarConnection.setUseCaches(false);
-                JarFile jarFile = jarConnection.getJarFile();
+	public static void agentmain(String args, Instrumentation instrumentation) {
+		new MainFrame(instrumentation);
+		Logger.log("[Agent] Successfully loaded into the JVM");
+		try {
 
-                instrumentation.appendToSystemClassLoaderSearch(jarFile);
+			JarFile jarFile = new JarFile(Constants.ABSOLUTE_JAR_PATH);
+			instrumentation.appendToSystemClassLoaderSearch(jarFile);
 
-                Class<?> clazz = ClassLoader.getSystemClassLoader().loadClass("main");
-                if(clazz != null) {
-                    Logger.log("WE LOADED");
-                } else {
+			Class<?> clazz = ClassLoader.getSystemClassLoader().loadClass("org.khal.khalcrafter.KhalAIOCrafter");
+			if (clazz != null) {
+				Logger.log("Class loader found.");
+			} else {
 
-                }
-            } catch (Exception e) {
-                Logger.log(e.getLocalizedMessage());
-            }
+			}
+		} catch (Exception e) {
+			Logger.log(e.getLocalizedMessage());
+		}
 
-    }
+	}
 
-    @Override
-    public byte[] transform(ClassLoader loader, String className,
-                            Class<?> redefinedClass, ProtectionDomain protDomain, byte[] classBytes) {
-        Logger.log("Adding: " + className + " to cache..");
-        return classBytes;
-    }
+	@Override
+	public byte[] transform(ClassLoader loader, String className,
+	                        Class<?> redefinedClass, ProtectionDomain protDomain, byte[] classBytes) {
+		Logger.log("Adding: " + className + " to cache..");
+		return classBytes;
+	}
 }
---
+++ src/org/osbot/jailbreak/data/Constants.java
@@ -0,0 +1,10 @@
+package org.osbot.jailbreak.data;
+
+import java.io.File;
+
+public class Constants {
+
+	public static final String LOCAL_JAR_PATH = System.getProperty("user.home") + File.separator + "Desktop";
+	public static final String LOCAL_JAR_NAME = "Khal Crafter.jar";
+	public static final String ABSOLUTE_JAR_PATH = LOCAL_JAR_PATH + File.separator + LOCAL_JAR_NAME;
+}
